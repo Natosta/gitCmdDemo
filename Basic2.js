7246ed3e156237a3b6497c72bf303ddab4c995cb
Natosta <anderfanx@gmail.com>
2024-06-13 11:38:30 +0100

    Adding my files

---
+++ Basic2.js
@@ -0,0 +1,17 @@
+
+console.log("Hello World")
+
+let a=4
+console.log(a)
+console.log(typeof(a))
+
+
+let b = 234.6 
+console.log(typeof(b))
+
+let c = "Anderfanx Test" 
+console.log(typeof(c))
+
+let required = true
+console.log(a)
+console.log(typeof(required))
